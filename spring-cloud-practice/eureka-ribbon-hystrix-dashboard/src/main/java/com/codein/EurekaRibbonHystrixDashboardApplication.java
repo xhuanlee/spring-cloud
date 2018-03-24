ba63c904e97623674ccae080b9af9dfa94dc440d
xhuanlee <lixhuan822@163.com>
2018-03-24 15:19:03 +0800

    add module eureka-zuul

---
+++ spring-cloud-practice/eureka-ribbon-hystrix-dashboard/src/main/java/com/codein/EurekaRibbonHystrixDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.codein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaRibbonHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaRibbonHystrixDashboardApplication.class, args);
+	}
+}
---
+++ spring-cloud-practice/eureka-zuul/src/main/java/com/codein/EurekaZuulApplication.java
@@ -0,0 +1,16 @@
+package com.codein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class EurekaZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaZuulApplication.class, args);
+	}
+}
