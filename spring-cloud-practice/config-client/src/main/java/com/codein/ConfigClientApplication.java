9f98c09398f91c5d0774c272a7e6a95151b64c5b
xhuanlee <lixhuan822@163.com>
2018-03-24 16:28:00 +0800

    add module config-client

---
+++ spring-cloud-practice/config-client/src/main/java/com/codein/ConfigClientApplication.java
@@ -0,0 +1,25 @@
+package com.codein;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@Value("${foo}")
+	private String foo;
+
+	@GetMapping("hi")
+	public String getServerConfig() {
+		return String.format("config value foo from git is: %s", foo);
+	}
+
+}
